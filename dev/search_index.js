9cac4cc84ce60f962f2ec0d8df2de9401b0dd0a2
Documenter.jl <documenter@juliadocs.github.io>
2021-05-15 13:06:02 +0000

    build based on 91d6cd4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BasisChange","category":"page"},{"location":"#BasisChange","page":"Home","title":"BasisChange","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BasisChange.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BasisChange]","category":"page"}]
+}
