0552b04ff72a84572096c7671450052d2219eec0
toma-vlad <tomapv@gmail.com>
2021-05-16 00:29:25 +0300

    Add LinearAlgebra as dep. Wrote main function.

--- src/BasisChange.jl
+++ src/BasisChange.jl
@@ -1,31 +1,10 @@
 module BasisChange
 
-using StaticArrays
+export rotation_matrix
+
 using LinearAlgebra
 
-tr(m) = transpose(m)
-
-e₁ = SVector(1., 0. ,0.)
-e₂ = SVector(0., 1. ,0.)
-e₃ = SVector(0., 0. ,1.)
-
-I = [tr(e₁); tr(e₂); tr(e₃)]
-
-function M(e₁′, e₃′)
-    @assert iszero(e₁′ ⋅ e₃′) "The input vectors are not orthogonal!"
-    v₁ᵀ = tr(e₁′)
-    v₃ᵀ = tr(e₃′)
-
-    v₂ᵀ = tr(e₃′ × e₁′)
-
-    A = [v₁ᵀ; v₂ᵀ; v₃ᵀ]
-    b₁ = I*e₁
-    b₂ = I*e₂
-    b₃ = I*e₃
-
-    return [tr(A\(b₁));
-            tr(A\(b₂));
-            tr(A\(b₃));]
-end
+rotation_matrix(B, B′) = B*transpose(B′)
 
 end
+ 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,20 @@
 using BasisChange
+using LinearAlgebra
 using Test
 
+δ(x,y) = (x == y ? 1. : 0.)   
+
 @testset "BasisChange.jl" begin
-    # Write your tests here.
+    @testset "Proper Transformation" begin
+        for N in 3:2:11 # even dimensions require minuses to keep handedness
+            B = I
+            for i in 1:N # couldn't be bothered to figure the formula out
+                B′ = [δ(j-1, (N + k - i)% N) for (k, j) in Iterators.product(1:N,1:N)]
+                R = rotation_matrix(B,B′)
+                Δ = det(R)
+                @test abs(Δ) ≈ 1. atol = 1e-15 
+                @test sign(Δ) == 1 
+            end
+        end
+    end
 end
